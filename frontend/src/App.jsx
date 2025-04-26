1eca9ef7fc00bef448a367ae5344ec7704de1097
bigboisw <wsean314@gmail.com>
2025-04-26 16:09:11 -0700

    restored app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,27 +6,19 @@ import NewsPage from './components/NewsQuiz/NewsPage';
 import Leaderboard from './components/Leaderboard/Leaderboard'
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 
-
-function App(){
-  return(
-    <Leaderboard />
-  )
-}
-
-
-/*
 function App() {
   return (
     <div>
       <Router>
-        <Routes> {/* Use <Routes> to wrap your <Route> components }
+        <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/newsquiz" element={<NewsPage />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
       </Router>
     </div>  
   );
 }
-*/
+
 
 export default App;
