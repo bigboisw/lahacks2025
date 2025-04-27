523400ee5ac08c9fe15e8b6c3e36d2fd6161d80a
t1ffanyc <tchen1515@gmail.com>
2025-04-26 21:49:09 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const jsonData = JSON.parse(
   )
 );
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3001' }));
 app.use(express.json());
 
 // Connect to the database
